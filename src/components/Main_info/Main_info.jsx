f398b71eefeb1033e18c932f61349f7930c6aed1
MaggieShpileva <63877500+MaggieShpileva@users.noreply.github.com>
2022-07-22 01:10:08 +0300

    Add to new change in Home page and About page.

--- src/components/Main_info/Main_info.jsx
+++ src/components/Main_info/Main_info.jsx
@@ -1,22 +1,24 @@
 import React from "react";
-import "./Main_info.css";
+import styles from "./MainInfo.module.scss";
 import Hero from "../../img/hero-right.png";
 export const Main_info = () => {
   return (
-    <div className="main-info">
-      <div className="conteiner">
-        <div className="text-conteiner">
-          <p className="info-p">5 TO 9 MAY 2019, MARDAVALL HOTEL, NEW YORK</p>
-          <h1 className="info_h1">
+    <div className={styles.main_info}>
+      <div className={styles.conteiner}>
+        <div className={styles.text_conteiner}>
+          <p className={styles.info_p}>
+            5 TO 9 MAY 2019, MARDAVALL HOTEL, NEW YORK
+          </p>
+          <h1 className={styles.info_h1}>
             Change Your Mind <br />
             To Become Sucess
           </h1>
-          <a href="#" className="button">
+          <a href="#" className={styles.button}>
             Buy Ticket
           </a>
         </div>
 
-        <img className="main-info-img" src={Hero} alt="" />
+        <img className={styles.main_img} src={Hero} alt="" />
       </div>
     </div>
   );
